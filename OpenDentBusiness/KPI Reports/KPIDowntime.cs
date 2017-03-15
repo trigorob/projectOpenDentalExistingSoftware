4d73263258c8cf70da95e5884919f0bfe646e6e9
J-Gachiri-Wamweya <gachiriwamweya@gmail.com>
2017-03-15 02:15:15 -0700

    OpenDentBusiness files KPI 6 and 7
    OpenDentBusiness\KPIDowntime & KPINonProductivePracticeTime

---
+++ OpenDentBusiness/KPI Reports/KPIDowntime.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+
+namespace OpenDentBusiness {
+	public class KPIDowntime {
+
+		///<summary> dateStart and dateEnd can be MinVal/MaxVal to indicate "forever".</summary>
+		public static DataTable GetDowntime(DateTime dateStart,DateTime dateEnd) {
+			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
+				return Meth.GetTable(MethodBase.GetCurrentMethod(),dateStart,dateEnd);
+			}
+			DataTable table=new DataTable();
+            table.Columns.Add("Provider Name");
+            table.Columns.Add("Provider Number");
+			table.Columns.Add("Total Down-time");
+			
+            DataRow row;
+            string command = @"
+            
+            SELECT  provider.MI,provider.LName,provider.FName,provider.ProvNum, sec_to_time((sum((CHAR_LENGTH(appointment.Pattern) - CHAR_LENGTH(REPLACE(appointment.Pattern, 'X', ''))) / CHAR_LENGTH('X')))*avg(apptview.RowsPerIncr)*60)  AS DTime
+				FROM appointment 
+                LEFT JOIN provider ON (provider.ProvNum = appointment.ProvNum)
+                CROSS JOIN apptview
+                WHERE EXISTS (	SELECT *
+								FROM procedurelog 
+                                WHERE procedurelog.AptNum = appointment.AptNum
+								AND procedurelog.CodeNum = 99998 or 99999
+                                )
+                AND appointment.AptStatus = 5
+				AND appointment.AptDateTime BETWEEN " + POut.DateT(dateStart) + @" AND " + POut.DateT(dateEnd) + @"
+                GROUP BY provider.ProvNum";
+
+            DataTable raw=ReportsComplex.GetTable(command);
+			Provider prov;
+			for(int i=0;i<raw.Rows.Count;i++) {
+				row=table.NewRow();
+				prov=new Provider();
+				prov.LName=raw.Rows[i]["LName"].ToString();
+				prov.FName=raw.Rows[i]["FName"].ToString();
+				prov.MI=raw.Rows[i]["MI"].ToString();
+				row["Provider Name"]=prov.GetFormalName();
+                row["Provider Number"]=raw.Rows[i]["ProvNum"].ToString();
+                row["Total Down-time"] = raw.Rows[i]["Dtime"].ToString();
+				table.Rows.Add(row);
+			}
+			return table;
+		}
+
+
+	}
+}
---
+++ OpenDentBusiness/KPI Reports/KPINonProductivePracticeTime.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+
+namespace OpenDentBusiness
+{
+    public class NonProductivePracticeTime
+    {
+
+        ///<summary> dateStart and dateEnd can be MinVal/MaxVal to indicate "forever".</summary>
+        public static DataTable GetNonProductivePracticeTime(DateTime dateStart, DateTime dateEnd)
+        {
+            if (RemotingClient.RemotingRole == RemotingRole.ClientWeb)
+            {
+                return Meth.GetTable(MethodBase.GetCurrentMethod(), dateStart, dateEnd);
+            }
+            DataTable table = new DataTable();
+            table.Columns.Add("Total Non-Productive Practice Time");
+            
+            DataRow row;
+            string command = @"
+            SELECT  sec_to_time(sum(length(appointment.Pattern))*avg(apptview.RowsPerIncr)*60) AS NonProdTime
+				FROM appointment 
+                LEFT JOIN patient ON (patient.PatNum = appointment.PatNum)
+                CROSS JOIN apptview
+                WHERE EXISTS (	SELECT *
+								FROM procedurelog 
+                                WHERE procedurelog.AptNum = appointment.AptNum
+								AND procedurelog.CodeNum = 99998 or 99999)
+                AND appointment.AptStatus = 5 
+				AND appointment.AptDateTime BETWEEN " + POut.DateT(dateStart) + @" AND " + POut.DateT(dateEnd) + @"";
+
+            DataTable raw = ReportsComplex.GetTable(command);
+            for (int i = 0; i < raw.Rows.Count; i++)
+            {
+                row = table.NewRow();
+                row["Total Non-Productive Practice Time"] = raw.Rows[i]["NonProdTime"].ToString();
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+
+
+    }
+}
+
